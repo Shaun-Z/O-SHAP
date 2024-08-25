aa6dc7c5e55ce16bc2e9fba11076f0af702fbe79
Shaun-Z <xiangyuzhou823@gmail.com>
2024-08-25 03:12:29 -0700

    Add test.py (requires revision)

--- datasets/imagenet_dataset.py
+++ datasets/imagenet_dataset.py
@@ -37,16 +37,16 @@ class ImageNetDataset(BaseDataset):
     def load_data(self):
         """Load data into memory.(here we only load the path to the data cuz the dataset is too large)"""
         # load data from /path/to/data/
+        self.X = []
+        self.Y = []
         if self.phase == 'train':
             dir = os.path.join(self.dir, self.phase) # directory to the train images
             directories = os.listdir(dir) # directories of the train images
-            self.X = []
-            self.Y = []
             for directory in directories:
                 images = glob(os.path.join(dir, directory, 'images', '*.JPEG')) # get the list of path to the train images
                 self.X += images
                 self.Y += [directory] * len(images) # current directory is the label of the images
-            self.Y_train = torch.tensor(pd.get_dummies(pd.Series(self.Y)).values, dtype=torch.float32) # one-hot encode the labels
+            self.Y_one_hot = torch.tensor(pd.get_dummies(pd.Series(self.Y)).values, dtype=torch.float32) # one-hot encode the labels
 
         elif self.phase == 'val':
             dir = os.path.join(self.dir, self.phase) # directory to the val images
@@ -55,6 +55,8 @@ class ImageNetDataset(BaseDataset):
                 content = file.readlines() # read the content of the val_annotations.txt file
             for image in self.X:
                 self.Y.append(content[int(image.split('_')[-1].split('.')[0])].split()[1]) # look up the label of each image in val_annotations.txt
+            self.Y_one_hot = torch.tensor(pd.get_dummies(pd.Series(self.Y)).values, dtype=torch.float32) # one-hot encode the labels
+            '''Revisions are required here'''
 
         else:
             dir = os.path.join(self.dir, self.phase, 'images') # directory to the test images
@@ -77,10 +79,9 @@ class ImageNetDataset(BaseDataset):
         path = self.X[index]
         im = Image.open(path).convert("RGB")
         X_tensor = transform(im) # .permute(1,2,0)
-        if self.phase == 'train':
-            return {'X': X_tensor, 'Y_train': self.Y_train[index], 'Y': self.Y[index]} # X_train : {num_batches, 64, 64, 3} , Y_train : a list of labels
-        elif self.phase == 'val':
-            return {'X': X_tensor, 'Y': self.Y[index]}
+
+        return {'X': X_tensor, 'Y_one_hot': self.Y_one_hot[index], 'Y': self.Y[index]} # X_tensor : {num_batches, 3, 64, 64} , Y_one_hot : a list of labels
+
 
     def __len__(self):
         """Return the total number of images in the dataset."""
---
+++ test.py
@@ -0,0 +1,20 @@
+from options.test_options import TestOptions
+from datasets import create_dataset
+from models import create_model
+import numpy as np
+
+opt = TestOptions().parse()  # get test options
+
+dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
+dataset_size = len(dataset)    # get the number of images in the dataset.
+print('The number of testing images = %d' % dataset_size)
+
+model = create_model(opt)      # create a model given opt.model and other options
+model.setup(opt)   
+
+for i, data in enumerate(dataset):
+    print(data['X'].shape, data['Y'])
+    model.set_input(data)
+    model.forward()
+    print(np.argmax(model.output.detach().numpy()))
+    exit()

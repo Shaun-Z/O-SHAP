4581f129c46fc2a05c5d0f53dc547eadcea1183b
Shaun-Z <xiangyuzhou823@gmail.com>
2024-08-27 18:35:06 -0700

    Change defalut resnet blocks to 50

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ for i, data in enumerate(dataloader):
     model.forward()
     predict_result = model.output.detach().numpy()
     index_max = np.argmax(predict_result)
-    print(data['Y'], labels[index_max], predict_result[0,index_max], index_max)  # print the true label and the predicted label
+    print(data['Y'], labels[index_max], data['Y'][0]==labels[index_max], index_max)  # print the true label and the predicted label
 '''
 python test.py -d ./data/tiny-imagenet -n RestNetClassifier -g -1 -m res_class --dataset_name imagenet --phase val 
 '''

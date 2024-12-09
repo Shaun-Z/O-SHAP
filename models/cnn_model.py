7a8812e116e0c3cd5d4e6419ba29bbd814afeb08
Shaun-Z <xiangyuzhou823@gmail.com>
2024-12-09 02:25:12 -0700

    Add __call__() function for convenience

--- models/cnn_model.py
+++ models/cnn_model.py
@@ -84,25 +84,6 @@ class CnnModel(BaseModel):
         self.backward()             # calculate gradients
         self.optimizer.step()       # update weights
 
-# class CnnClassifier(nn.Module):
-#     def __init__(self, num_classes, dropout=0.5):
-#         super(CnnClassifier, self).__init__()
-#         self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
-#         self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
-#         self.conv2_drop = nn.Dropout2d(p=dropout)
-#         self.fc1 = nn.Linear(1600, 100) # 1600 = number channels * width * height
-#         self.fc2 = nn.Linear(100, 10)
-#         self.fc1_drop = nn.Dropout(p=dropout)
-#         # self.cnn = cnn(dropout)
-#         # self.fc = nn.Linear(10, num_classes)
-
-#     def forward(self, x):
-#         x = torch.relu(F.max_pool2d(self.conv1(x), 2))
-#         x = torch.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-
-#         # flatten over channel, height and width = 1600
-#         x = x.view(-1, x.size(1) * x.size(2) * x.size(3))
-
-#         x = torch.relu(self.fc1_drop(self.fc1(x)))
-#         x = torch.softmax(self.fc2(x), dim=-1)
-#         return x
+    def __call__(self, input):
+        """Return the output of the model"""
+        return self.netCnn_classifier(input.to(self.device))

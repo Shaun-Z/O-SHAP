03edfda6cfcda95b7f3b032fa9d68303602a0a03
Shaun-Z <xiangyuzhou823@gmail.com>
2024-12-18 23:54:28 -0700

    Add a script to test xxxx_dataset.py

---
+++ dataset_test.py
@@ -0,0 +1,32 @@
+'''
+To test this script, run the following command:
+----------------
+python imagenet_dataset_test.py --dataroot ./data/tiny-imagenet --gpu_ids -1
+----------------
+or
+----------------
+python imagenet_dataset_test.py -d ./data/tiny-imagenet -g -1
+----------------
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+
+from options.train_options import TrainOptions
+from options.test_options import TestOptions
+from datasets.imagenet_dataset import ImageNetDataset
+
+from datasets import create_dataset
+
+if __name__ == '__main__':
+    
+    opt = TestOptions().parse()
+    dataset = create_dataset(opt)
+
+    print(dataset.labels)
+
+    exit()
+
+    for i in range(len(dataset)):
+        data = dataset[i]
+        print(f"X:{data['X'].shape}\tlabel:{data['label']}\tindices:{data['indices']}")
+

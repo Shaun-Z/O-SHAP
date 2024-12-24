c8f94f8ba50d9ac296f96f19ae76890ebc280daf
Shaun-Z <xiangyuzhou823@gmail.com>
2024-12-23 18:41:50 -0700

    Adapt this file to boundings

--- dataset_test.py
+++ dataset_test.py
@@ -46,7 +46,6 @@ from PIL import Image
 
 from options.train_options import TrainOptions
 from options.test_options import TestOptions
-from datasets.imagenet_dataset import ImageNetDataset
 
 from datasets import create_dataset
 
@@ -55,26 +54,27 @@ if __name__ == '__main__':
     dataset = create_dataset(opt)
 
     # Load the data item with index 200
-    index = 100
+    index = 0
     data = dataset[index]  # Load the 200th sample
 
     # Extract normalized image and box_map
     normalized_image = data['X']  # Tensor for the normalized image
-    box_map = data['mask']  # Tensor for the box map
+    # mask = data['mask']  # Tensor for the box map
+    bounding = data['bounding']  # Tensor for the bounding box
 
     # Perform inverse normalization (recover the original image)
     inv_image = dataset.inv_transform(normalized_image)  # Reverse the normalization
     image_pil = ToPILImage()(inv_image)  # Convert to PIL image
 
     # Convert box_map tensor to PIL image
-    box_map_pil = ToPILImage()(box_map)
+    bounding_pil = ToPILImage()(bounding)  # Convert to PIL image
 
     if image_pil.mode != 'RGBA':
         image_pil = image_pil.convert('RGBA')
-    if box_map_pil.mode != 'RGBA':
-        box_map_pil = box_map_pil.convert('RGBA')
+    if bounding_pil.mode != 'RGBA':
+        bounding_pil = bounding_pil.convert('RGBA')
     # Overlay box_map on the original image
-    overlay = Image.alpha_composite(image_pil, box_map_pil)
+    overlay = Image.alpha_composite(image_pil, bounding_pil)
 
     # Display the combined image
     plt.figure(figsize=(8, 8))
@@ -82,4 +82,3 @@ if __name__ == '__main__':
     plt.title(f"Label: {data['label']}, Indices: {data['indices']}")
     plt.axis("off")
     plt.show()
-

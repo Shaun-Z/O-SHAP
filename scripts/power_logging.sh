64cf27072b15b04d33ee8c2972f2740f3b9fbe96
Shaun-Z <xiangyuzhou823@gmail.com>
2025-01-29 16:23:31 -0700

    Add column index 'time'

--- scripts/power_logging.sh
+++ scripts/power_logging.sh
@@ -1,14 +1,14 @@
-# 启动两个命令并将它们放在后台运行
+# Lauch two commands in parallel, one is training a model, the other is logging GPU information.
 python train.py --config config/ResNet50onEuroSAT/train.yaml &
 pid1=$!
-stdbuf -oL nvidia-smi --query-gpu=index,name,memory.total,memory.used,memory.free,utilization.gpu,utilization.memory,temperature.gpu,power.draw,power.limit --format=csv -l 1 | while IFS= read -r line; do
+stdbuf -oL nvidia-smi --query-gpu=time,index,name,memory.total,memory.used,memory.free,utilization.gpu,utilization.memory,temperature.gpu,power.draw,power.limit --format=csv -l 1 | while IFS= read -r line; do
     echo "$(date '+%Y-%m-%d %H:%M:%S'), $line"
 done >> gpu_full_log.csv &
 pid2=$!
 
-# 等待其中一个命令结束
+# Wait for both commands to finish
 wait -n $pid1 $pid2
 
-# 终止另一个命令
+# Kill the remaining process
 kill $pid1 $pid2
 

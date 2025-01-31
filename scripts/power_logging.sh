46821315a8958db06d4750c8a0e1cdfd59541cf5
Shaun-Z <xiangyuzhou823@gmail.com>
2025-01-30 22:23:29 -0700

    Modify error

--- scripts/power_logging.sh
+++ scripts/power_logging.sh
@@ -1,9 +1,14 @@
+#!/bin/bash
+
+# Define the output file
+output_file="gpu_full_log.csv"
+
 # Lauch two commands in parallel, one is training a model, the other is logging GPU information.
-python train.py --config config/ResNet50onEuroSAT/train.yaml &
-pid1=$!
-stdbuf -oL nvidia-smi --query-gpu=time,index,name,memory.total,memory.used,memory.free,utilization.gpu,utilization.memory,temperature.gpu,power.draw,power.limit --format=csv -l 1 | while IFS= read -r line; do
+stdbuf -oL nvidia-smi --query-gpu=index,name,memory.total,memory.used,memory.free,utilization.gpu,utilization.memory,temperature.gpu,power.draw,power.limit --format=csv -l 1 | while IFS= read -r line; do
     echo "$(date '+%Y-%m-%d %H:%M:%S'), $line"
-done >> gpu_full_log.csv &
+done >> $output_file &
+pid1=$!
+python train.py --config config/ResNet50onEuroSAT/train.yaml &
 pid2=$!
 
 # Wait for both commands to finish
@@ -12,3 +17,5 @@ wait -n $pid1 $pid2
 # Kill the remaining process
 kill $pid1 $pid2
 
+# Modify the first cell of the header to 'time'
+sed -i '1s/^[^,]*/time/' $output_file

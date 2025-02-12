47d161d4b928468571ff23a4d4f5cb9839c31306
administrator <xzhou185@asu.edu>
2025-02-12 19:22:52 +0000

    Remove existing file before run

--- scripts/power_logging.sh
+++ scripts/power_logging.sh
@@ -3,6 +3,8 @@
 # Define the output file
 output_file="gpu_full_log.csv"
 
+rm -f $output_file
+
 # Lauch two commands in parallel, one is training a model, the other is logging GPU information.
 stdbuf -oL nvidia-smi --query-gpu=index,name,memory.total,memory.used,memory.free,utilization.gpu,utilization.memory,temperature.gpu,power.draw,power.limit --format=csv -l 1 | while IFS= read -r line; do
     echo "$(date '+%Y-%m-%d %H:%M:%S'), $line"
